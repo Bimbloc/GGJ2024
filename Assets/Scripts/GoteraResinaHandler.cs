024d4493367dbf4f21c4a33e65d1088ee578fc9f
MiguelCGa <micurros@ucm.es>
2024-01-27 21:46:30 +0100

    Puzle goteras épicas

---
+++ Assets/Scripts/GoteraResinaHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GoteraResinaHandler : MonoBehaviour
+{
+    GameObject goteraObject;
+    GameObject pegoteObject;
+    public bool completed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goteraObject = transform.GetChild(0).gameObject;
+        pegoteObject = transform.GetChild(1).gameObject;
+        pegoteObject.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<BarrelComponent>() != null)
+        {
+            complete();
+        }
+    }
+
+    void complete()
+    {
+        goteraObject.SetActive(false);
+        pegoteObject.SetActive(true);
+        completed = true;
+    }
+    public void reset()
+    {
+
+        goteraObject.SetActive(true);
+        pegoteObject.SetActive(false);
+        completed = false;
+    }
+}
--- Assets/Scripts/Puzzle1Manager.cs
+++ Assets/Scripts/Puzzle1Manager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Puzzle1Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GoteraResinaHandler[] goteras;
+
+    int next = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        int i = 0;
+        foreach (var gotera in goteras)
+        {
+            if (gotera.completed)
+            {
+                if (i > next)
+                {
+                    reset();
+                    break;
+                }
+                else if (i == next) 
+                {
+                    ++next;
+                }
+            }
+            ++i;
+        }
+
+        if (next == goteras.Length)
+        {
+            complete();
+        }
+    }
+
+    void complete()
+    {
+        GameManager.GetInstance().CompletePuzzle(1);
+        this.enabled = false;
+    }
+
+    void reset()
+    {
+        foreach (var gotera in goteras)
+        {
+            gotera.reset();
+            next = 0;
+        }
     }
 }
